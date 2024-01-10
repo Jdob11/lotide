d3f46b5a4655dbb32d410af4f4093ac79f4a9451
Jeff Dobson <jbdobson11@gmail.com>
2024-01-10 09:40:33 -0800

    removed failing test cases

--- without.js
+++ without.js
@@ -41,12 +41,6 @@ assertArraysEqual(without([1, 2, 3, 4, 5], [2, 4]), [1, 3, 5]);
 assertArraysEqual(without([], [1, 2, 3]), []);
 assertArraysEqual(without([1, 2, 3], []), [1, 2, 3]);
 assertArraysEqual(without([], []), []);
-assertArraysEqual(without([1, 2, 3], [2]), [2, 3]);
-assertArraysEqual(without(["1", "2", "3"], ["1", 2, "3"]), ["1", "2"]);
-assertArraysEqual(without([1, 2, 3, 4, 5], [2, 4]), [1, 3, 4, 5]);
-assertArraysEqual(without([], [1, 2, 3]), [1, 2, 3]);
-assertArraysEqual(without([1, 2, 3], [3]), [1, 2, 3]);
-assertArraysEqual(without([], []), [1]);
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
