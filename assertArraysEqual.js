3e8fb89172fb29a212ba32015fdc0a37e9261ab0
Jeff Dobson <jbdobson11@gmail.com>
2024-01-09 19:25:29 -0800

    added comments

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,9 @@
-/*const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-
-};
-*/
-
 const assertArraysEqual = function(arr1, arr2) {
   //confirming arrays are an equal length
   if (arr1.length !== arr2.length) {
     console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`);
   } else {
-  //loop to compare that individual array elements are the same
+  //loop to compare that individual array elements are the same and break loop if they aren't
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
         console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`);
