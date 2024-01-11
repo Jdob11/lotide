b5d7345c99c148e93655a1116921a6c1883406ee
Jeff Dobson <jbdobson11@gmail.com>
2024-01-10 19:32:31 -0800

    added condition to skip spaces and added asserEqual tests

--- countLetters.js
+++ countLetters.js
@@ -11,12 +11,20 @@ const countLetters = function(sentence) {
   //create empty object to store results
   const result = {};
   for (const letter of sentence) {
+    if (letter !== " ") {
       if (result[letter]) {
         result[letter] += 1;
       } else {
         result[letter] = 1;
       }
     }
-    return result;
   }
+  return result;
+};
 
+
+const result1 = countLetters("lighthouse in the house");
+
+assertEqual(result1["l"], 1);
+assertEqual(result1["i"], 2);
+assertEqual(result1["e"], 3);
