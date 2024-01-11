86dd64540a9fe7e6914655b4b705aa2fa69178be
Jeff Dobson <jbdobson11@gmail.com>
2024-01-10 23:38:02 -0800

    changed isArray values to object[key] instead of object

--- eqObjects.js
+++ eqObjects.js
@@ -34,7 +34,7 @@ const eqObjects = function(object1, object2) {
     if (object1[key] !== object2[key]) {
       return false;
     }
-    if (Array.isArray(object1) && Array.isArray(object2)) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       eqArrays(object1, object2);
     }
   }
