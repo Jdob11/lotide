e26797952747c957bd606d415bae1d1c165f7fbc
Jeff Dobson <jbdobson11@gmail.com>
2024-01-10 20:37:14 -0800

    created function to give index position of letters in a sentence

---
+++ letterPositions.js
@@ -0,0 +1,45 @@
+
+const eqArrays = function(arr1, arr2) {
+  //confirming arrays are an equal length
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  //loop to compare that individual array elements are the same
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//this function compares if two arrays are equal and sends a pass of fail message
+const assertArraysEqual = function(actual, expected) {
+  let isEqual = eqArrays(actual, expected);
+  if (isEqual) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(1);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
+
