943c02cdd04dd4afb64108836aa3e81885b78edf
Jeff Dobson <jbdobson11@gmail.com>
2024-01-11 21:55:54 -0800

    readded test to cofirm original array was not mutated

--- test/tailTest.js
+++ test/tailTest.js
@@ -8,7 +8,9 @@ assertEqual(tail(words).length, 2);
 assertEqual(tail(words)[0], "Lighthouse");
 //checking new index value at 1 is the original index value 2
 assertEqual(tail(words)[1], "Labs");
-//confirming return of empty array if one or no values are given
+//confirming original array was not modified by tail
+assertEqual(words.length, 3);
+//confirming that when given one or no elements, an empty array is returned
 const moreWords = ["More"];
 const mostWords = [];
 assertEqual(tail(moreWords).length, 0);
