0d0c36e21fd1904ab84735149e4317d0a8538b3f
Jeff Dobson <jbdobson11@gmail.com>
2024-01-11 23:59:13 -0800

    refactors to test with mocha/chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,29 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
-//testing that given an odd numbered array, the middle value is returned
-assertArraysEqual(middle([1, 2, 3]), [2]);
-//testing that given an equal number array, the two most middle values are returned
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-//testing that given a single value array, and empty array is returned
-assertArraysEqual(middle([1]), []);
-//testing that given an empty array an empty array is returned
-assertArraysEqual(middle([]), []);
-//testing that given a two value array an empty array is returned
-assertArraysEqual(middle([1, 2]), []);
+
+describe("#middle", () => {
+
+  it("returns middle value if given odd numbered array", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns two middle values when given an even numbered array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); 
+  });
+
+  it("returns empty array given a single value array", () => {
+    assert.deepEqual(middle([1]), []); 
+  });
+
+  it("returns an empty array given an empty array", () => {
+    assert.deepEqual(middle([]), []);
+  });
+
+  it("returns an empty array when given a two value array", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+});
+
