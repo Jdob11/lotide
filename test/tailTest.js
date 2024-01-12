5dc0fdd8a2ab442251abfc7cf8e746f82a0bc40e
Jeff Dobson <jbdobson11@gmail.com>
2024-01-11 23:37:10 -0800

    refactored tail test for mocha/chai testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,37 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+// const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-//comparing expected output length to actual
-assertEqual(tail(words).length, 2);
-//checking new index value at 0 is the original index value 1
-assertEqual(tail(words)[0], "Lighthouse");
-//checking new index value at 1 is the original index value 2
-assertEqual(tail(words)[1], "Labs");
-//confirming original array was not modified by tail
-assertEqual(words.length, 3);
-//confirming that when given one or no elements, an empty array is returned
-const moreWords = ["More"];
-const mostWords = [];
-assertEqual(tail(moreWords).length, 0);
-assertEqual(tail(mostWords).length, 0);
+describe("#tail", () => {
+
+  it("returns array of proper length", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.strictEqual(tail(words).length, 2);
+  });
+
+  it("returns proper new value at array position 0", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.strictEqual(tail(words)[0], "Lighthouse"); 
+  });
+
+  it("returns proper new value at array position 1", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.strictEqual(tail(words)[1], "Labs");
+  });
+
+  it("confirms original array was not mutatued by tail", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.strictEqual(words.length, 3);
+  });
+
+  it("confirms when given one element an empty array is returned", () => {
+    const moreWords = ["More"];
+    assert.deepEqual(tail(moreWords), []);
+  });
+
+  it("confirms when given no elements an empty array is returned", () => {
+    const mostWords = [];
+    assert.deepEqual(tail(mostWords), []);
+  });
+
+});
