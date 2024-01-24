def14bdbab7585ea3b681a6429c4106435dcb50a
Jdob11 <jbdobson11@gmail.com>
2024-01-24 09:55:37 -0800

    created variables to hold sorted keys so array comparison would function properly

--- eqObjects.js
+++ eqObjects.js
@@ -23,15 +23,20 @@ const eqArrays = function(arr1, arr2) {
 
 //a function to compare if two objects are equal
 const eqObjects = function(object1, object2) {
+  //assign the keys of each object and sort them
+  let keys1 = Object.keys(object1).sort();
+  let keys2 = Object.keys(object2).sort();
   //compare if each object has the same number of keys
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
+  if (keys1.length !== keys2.length) {
     return false;
   }
-  //loop the keys in object1
-  for (let key in object1) {
+  //loop the sorted keys in object1
+  for (let key in keys1) {
     //if the value of the current key is an array, run eqArrays on both arrays to compare if they're equal
     if (Array.isArray(object1[key])) {
-      return eqArrays(object1[key], object2[key]);
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
     }
     //otherwise, compare the values and return false if they aren't equal
     if (object1[key] !== object2[key]) {
