8595bfb609de6da44c9464b7627017d98ca5c56c
Jdob11 <jbdobson11@gmail.com>
2024-01-24 02:49:52 -0800

    wrote function to count letters in given string

--- countLetters.js
+++ countLetters.js
@@ -1,30 +1,86 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-//   }
-
-// };
-
-const countLetters = function(sentence) {
-  //create empty object to store results
-  const result = {};
-  for (const letter of sentence) {
-    if (letter !== " ") {
-      if (result[letter]) {
-        result[letter] += 1;
-      } else {
-        result[letter] = 1;
-      }
-    }
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
   }
-  return result;
+
 };
 
-module.exports = countLetters;
-// const result1 = countLetters("lighthouse in the house");
+const countLetters = function(string) {
+  //create object to contain final counts
+  let results = {};
+  //loop each letter of string
+  for (const letter of string) {
+    //add current letter to results object if it isn't there already, increase count by one if it is
+    if (letter !== ' ')
+      results[letter] = (results[letter] || 0) + 1;
+  }
+  //return results object with final counts
+  return results;
+};
 
-// assertEqual(result1["l"], 1);
-// assertEqual(result1["i"], 2);
-// assertEqual(result1["e"], 3);
+
+
+const result1 = countLetters("lighthouse in the house");
+
+assertEqual(result1["l"], 1);
+assertEqual(result1["i"], 2);
+assertEqual(result1["e"], 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const countLetters = function(sentence) {
+//   //create empty object to store results
+//   const result = {};
+//   for (const letter of sentence) {
+//     if (letter !== " ") {
+//       if (result[letter]) {
+//         result[letter] += 1;
+//       } else {
+//         result[letter] = 1;
+//       }
+//     }
+//   }
+//   return result;
+// };
+
+// module.exports = countLetters;
+
