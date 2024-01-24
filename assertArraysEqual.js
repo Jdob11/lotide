bb615cba5940dcc621633885b2b8360f20b5bddb
Jdob11 <jbdobson11@gmail.com>
2024-01-23 19:29:40 -0800

    used eqArrays function as helper to make assertArraysEqual to compare 2 given arrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,19 @@
-const eqArrays = require('./eqArrays');
+// const eqArrays = require('./eqArrays');
+
+const eqArrays = function(arr1, arr2) {
+  //confirming arrays are an equal length
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  //loop to compare that individual array elements are the same
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 
 //this function compares if two arrays are equal and sends a pass of fail message
 const assertArraysEqual = function(actual, expected) {
@@ -11,4 +26,4 @@ const assertArraysEqual = function(actual, expected) {
 
 };
 
-module.exports = assertArraysEqual;
+// module.exports = assertArraysEqual;
