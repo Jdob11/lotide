f67ed2d2befb12ee6e530ebcc97ac143cb30830b
Jdob11 <jbdobson11@gmail.com>
2024-01-24 05:21:27 -0800

    created function to return key of given value from given object

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,12 +7,12 @@ const assertEqual = function(actual, expected) {
 };
 
 const findKeyByValue = function(object, value) {
-  const keysOfObject = Object.keys(object);
-  for (let key of keysOfObject) {
+  for (let key in object) {
     if (object[key] === value) {
       return key;
     }
   }
+
 };
 
 const bestTVShowsByGenre = {
@@ -24,3 +24,10 @@ const bestTVShowsByGenre = {
 assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "the expanse"), undefined);
+assertEqual(findKeyByValue({ key1: 123, key2: null, key3: true }, 123), "key1");
+assertEqual(findKeyByValue({}, "Some Value"), undefined);
+assertEqual(findKeyByValue({ key1: "value1", key2: "value2" }, "value1"), "key1");
+assertEqual(findKeyByValue({ key1: "value1", key2: "value2" }, "value3"), undefined);
+assertEqual(findKeyByValue({ key1: undefined, key2: null, key3: "value" }, null), "key2");
