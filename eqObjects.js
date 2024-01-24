5363fdc318b6e71f7edd6d865eb065b31c3d5f74
Jdob11 <jbdobson11@gmail.com>
2024-01-24 10:56:26 -0800

    moved final value comparison into else statement so it only runs if the isArray statement doesnt

--- eqObjects.js
+++ eqObjects.js
@@ -23,24 +23,24 @@ const eqArrays = function(arr1, arr2) {
 
 //a function to compare if two objects are equal
 const eqObjects = function(object1, object2) {
-  //assign the keys of each object and sort them
-  let keys1 = Object.keys(object1).sort();
-  let keys2 = Object.keys(object2).sort();
+  //assign the keys of each object
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
   //compare if each object has the same number of keys
   if (keys1.length !== keys2.length) {
     return false;
   }
   //loop the sorted keys in object1
-  for (let key in keys1) {
+  for (let key in object1) {
     //if the value of the current key is an array, run eqArrays on both arrays to compare if they're equal
-    if (Array.isArray(object1[key])) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
-    }
-    //otherwise, compare the values and return false if they aren't equal
-    if (object1[key] !== object2[key]) {
-      return false;
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
     }
   }
   //return true if none of the other comparisons return false
