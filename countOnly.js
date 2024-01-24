0e00dbdd88268bc4301ec9bb965fce78f8853a80
Jdob11 <jbdobson11@gmail.com>
2024-01-24 02:40:29 -0800

    added comments to explain code

--- countOnly.js
+++ countOnly.js
@@ -8,12 +8,17 @@ const assertEqual = function(actual, expected) {
 };
 
 const countOnly = function(allItems, itemsToCount) {
+  //create object to contain final counts
   let results = {};
+  //loop each item of allItems object
   for (const item of allItems) {
+    //check if current item is in itemsToCount object
     if (itemsToCount[item]) {
+      //add current item to results object if it isn't there already, increase count by one if it is
       results[item] = (results[item] || 0) + 1;
     }
   }
+  //return results object with final counts
   return results;
 };
 
