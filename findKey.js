40157c4ffdf4a525476bc21fef1e82523cefcff9
Jdob11 <jbdobson11@gmail.com>
2024-01-31 16:10:32 -0800

    simplified comments

--- findKey.js
+++ findKey.js
@@ -9,13 +9,10 @@ const assertEqual = function(actual, expected) {
 
 //function to return first key that matches callback value
 const findKey = function(object, callback) {
-  //loop through keys of given object
   for (const key in object) {
-    //for current key, set 'value' to the value of that key
     const value = object[key];
-    //set result as callback with parameter of the value of the current key
     const result = callback(value);
-    //if callback with current keys value returns true, then value given to callback function is the same as the current keys value, so return current key
+    //if callback returns true, return current key
     if (result) {
       return key;
     }
