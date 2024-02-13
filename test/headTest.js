69b72cbebeea1a94c2974d322f811af8d8c7e3b5
Jdob11 <jbdobson11@gmail.com>
2024-02-13 13:46:53 -0800

    removed unecessary require from head, and failing tests from headTest

--- test/headTest.js
+++ test/headTest.js
@@ -3,7 +3,5 @@ const assertEqual = require('../assertEqual')
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5,6,7]), 6);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
 assertEqual(head([5,6,7]), 5);
 assertEqual(head([]), undefined);
