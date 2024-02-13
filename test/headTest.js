6415f7567855101328939b207735aa7d1a02a1f4
Jdob11 <jbdobson11@gmail.com>
2024-02-13 13:48:29 -0800

    removed duplicate test from headTest

--- test/headTest.js
+++ test/headTest.js
@@ -3,5 +3,4 @@ const assertEqual = require('../assertEqual')
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5,6,7]), 5);
 assertEqual(head([]), undefined);
