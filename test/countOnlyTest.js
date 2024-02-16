99836ee5be9d2a30db7667b47bcd497e8bd2d66c
Jdob11 <jbdobson11@gmail.com>
2024-02-16 03:24:29 -0800

    refactored to use mocha and chai

---
+++ test/countOnlyTest.js
@@ -0,0 +1,36 @@
+const assert = require('chai').assert;
+const _ = require('../index');
+
+describe("#countOnly", () => {
+
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+
+  const result1 = _.countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+  it("should return 1 when input is \"Jason\"", () => {
+    assert.strictEqual(result1["Jason"], 1);
+  });
+
+  it("should return undefined when input is \"Karima\"", () => {
+    assert.strictEqual(result1["Karima"], undefined);
+  });
+
+  it("should return 2 when input is \"Fang\"", () => {
+    assert.strictEqual(result1["Fang"], 2);
+  });
+
+  it("should return undefined when input is \"Agouhanna\"", () => {
+    assert.strictEqual(result1["Agouhanna"], undefined);
+  });
+
+});
