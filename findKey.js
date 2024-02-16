613224a6a3485f1be3152cfdfd08b371cac8f53f
Jdob11 <jbdobson11@gmail.com>
2024-02-16 04:36:30 -0800

    refactored to use mocha and chai

--- findKey.js
+++ findKey.js
@@ -1,12 +1,3 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-//   }
-
-// };
-
 //function to return first key that matches callback value
 const findKey = function(object, callback) {
   for (const key in object) {
@@ -20,11 +11,3 @@ const findKey = function(object, callback) {
 }
 
 module.exports = findKey;
-// assertEqual(findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri": { stars: 3 },
-//   "noma": { stars: 2 },
-//   "elBulli": { stars: 3 },
-//   "Ora": { stars: 2 },
-//   "Akelarre": { stars: 3 }
-// }, x => x.stars === 2), "noma");
