d08cba1acf900ac2e22da34166b602b99380f004
Jdob11 <jbdobson11@gmail.com>
2024-02-16 03:25:57 -0800

    moved result variable outside of individual tests

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -3,18 +3,17 @@ const _ = require('../index');
 
 describe("#countLetters", () => {
 
+  const result1 = _.countLetters("lighthouse in the house");
+
   it("should return 1 when input is \"l\"", () => {
-    const result1 = _.countLetters("lighthouse in the house");
     assert.strictEqual(result1["l"], 1);
   });
 
   it("should return 2 when input is \"i\"", () => {
-    const result1 = _.countLetters("lighthouse in the house");
     assert.strictEqual(result1["i"], 2);
   });
 
   it("should return 3 when input is \"e\"", () => {
-    const result1 = _.countLetters("lighthouse in the house");
     assert.strictEqual(result1["e"], 3);
   });
 
